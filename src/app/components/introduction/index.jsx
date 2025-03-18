cd6f93ab241c7bba9d287c1199da1badf3929a9b
Maria Eduarda <mariaeduardareistds2.senai@gmail.com>
2025-03-18 16:22:16 -0300

    feat: create introduction component

---
+++ src/app/components/introduction/index.jsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import styles from "./introduction.module.css";
+
+const Introduction = () => {
+    return (
+        <section className={styles.containerIntroduction}>
+            <h2>Colocar uma breve descrição sobre a representação dos icones aqui, ou um slogan</h2>
+            <div className={styles.cardIntroduction}>
+              <div className={styles.cardItem}>
+                <Image className={styles.logo} src={"/alvo.png"} alt='Icone de alvo' width={62} height={62}/>
+                <p>Capacitar as mulheres a se encontrarem através do autocuidado.</p>
+              </div>
+
+              <div className={styles.cardItem}>
+                <Image className={styles.logo} src={"/olho.png"} alt='Icone de olho' width={62} height={62}/>
+                <p>Ser o blog de referência no mercado de autocuidado.</p>
+              </div>
+
+              <div className={styles.cardItem}>
+                <Image className={styles.logo} src={"/diamante.png"} alt='Icone de diamante' width={62} height={62}/>
+         
+                  <p>Autocuidado, Inclusão, Empoderamento feminino</p>
+                           
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default Introduction;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header";
 import TopHeader from "./components/topHeader";
+import Introduction from "./components/introduction";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <TopHeader />
-      <Header />
-    </div>
+    <>
+      <div className={styles.container}>
+        <TopHeader />
+        <Header />
+      </div>
+      <div>
+        <Introduction /> 
+      </div>
+    </>
   );
 }
