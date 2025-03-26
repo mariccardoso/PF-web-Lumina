e4ffa7c2b7737123a9840a16b9b971fb120491d7
Maria Eduarda <mariaeduardareisTDS2.senai@gmail.com>
2025-03-26 19:08:00 -0300

    feat:Added layout for beauty section with images and style adjustments

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,30 @@ export default function Home() {
       <Header />
       <Introduction />
 
+      <div className={styles.beautyContainer}>
+      <Image className={styles.imgContent} src="/beautySection.png" alt="três mulheres" width={700} height={650} />
+        <div className={styles.beautyContent}>
+          <h2>Colocar título aqui</h2>
+          <p>Colocar descrição curta aqui</p>
+          <div className={styles.beautyCards}>
+            <div className={styles.beautyCard}>
+            <Image src="/cabelo.png" alt="icon de cabelo" width={110} height={110} />
+              <h3 className={styles.textBeauty}>Cabelo</h3>
+            </div>
+
+            <div className={styles.beautyCard}>
+            <Image src="/maquiagem.png" alt="icon de um rosto com maquiagem"width={110} height={110} />
+              <h3 className={styles.textBeauty}>Maquiagem</h3>
+            </div>
+
+            <div className={styles.beautyCard}>
+            <Image src="/skincare.png" alt="icon de um pote de creme" width={110} height={110} />              
+            <h3  className={styles.textBeauty}>Skincare</h3>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </section>
   );
-}
+};
