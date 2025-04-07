076fb2f792e64930b2ae9b4393114ffbe0602d71
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2025-04-07 17:15:15 -0300

    feat: Configure image domains in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['media.canva.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
