53bec7bcedd827d6a87aec9cb1fcdedb79e9949e
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2025-04-07 18:02:37 -0300

    feat: Migrate configuration from next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['media.canva.com'],
+  },
+};
+
+module.exports = nextConfig; // ou `export default nextConfig;` se estiver usando `.mjs`
