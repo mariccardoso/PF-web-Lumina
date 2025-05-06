46c036b2e719a3e4b627c692b43138473d961083
Sophia Gomes <sophiagomes.tds2.senai@gmail.com>
2025-05-06 08:53:30 -0300

    feat: create VideoSection, page maquiagem

---
+++ src/components/videoSectionRight/index.jsx
@@ -0,0 +1,38 @@
+import styles from "./videoSection.module.css";
+
+const VideoSectionRight = ({ videoSrc, title, description, pdfLink, buttonText }) => {
+  return (
+    <div className={styles.videoSectionContainer}>
+      <div className={styles.videoAndDescription}>
+        {/* Vídeo */}
+        <div className={styles.videoContainer}>
+          <iframe
+            src={videoSrc}
+            title="Vídeo tutorial"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        {/* Descrição e Botão */}
+        <div className={styles.textContainer}>
+          <p className={styles.title}>{title}</p>
+          <p className={styles.description}>{description}</p>
+          <div className={styles.buttonContainer}>
+            <a
+              href={pdfLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.button}
+            >
+              {buttonText}
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoSectionRight;
